3ca12cbade418bd9f15c1bf7c7b1055dcd55b04d
AleksandraTodorovich <aleksandratodorovic84@gmail.com>
2022-01-06 20:08:25 +0100

    Three static arrays edit

---
+++ src/projekat_1/pasta_bar.java
@@ -0,0 +1,20 @@
+package projekat_1;
+
+import java.util.Scanner;
+
+public class pasta_bar {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Izvolite!");
+		String [] stalneMusterije = {"0631111111", "063222222", "063333333", "064444444",
+				"065555555", "066666666"};
+		String [] niz = {"Makarone", "Spagete", "Bolognese", "Curetina", "Govedja prsuta", 
+		"Slanina", "Piletina", "4 sira", "Dimljeni sir", "Parmezan", "Pavlaka", "Pesto sos",
+		"Napolitana", "Povrce mix", "Pecurke", "Kutija"};
+		int [] cene = {50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20};
+	}
+
+}
